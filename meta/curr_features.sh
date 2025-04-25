e4299f02375cb29d61815d04a2cbd61102165a0f
Mohamed Taher <mohamed.taher0x3@gmail.com>
2025-04-25 15:17:49 +0300

    Added inital curr_features

---
+++ meta/curr_features.sh
@@ -0,0 +1,64 @@
+# Result                   
+TARGET                       
+# Loan
+NAME_CONTRACT_TYPE       
+AMT_CREDIT                   
+AMT_ANNUITY                  
+AMT_GOODS_PRICE              
+# Client
+CODE_GENDER                  
+FLAG_OWN_CAR                 
+FLAG_OWN_REALTY              
+NAME_HOUSING_TYPE            
+DAYS_BIRTH -> it is -ve value       
+NAME_EDUCATION_TYPE          
+# Client - family
+CNT_CHILDREN                 
+NAME_FAMILY_STATUS           
+CNT_FAM_MEMBERS              
+# Client - job
+AMT_INCOME_TOTAL             
+NAME_INCOME_TYPE             
+ORGANIZATION_TYPE            
+DAYS_EMPLOYED                
+# Client - connect
+DAYS_LAST_PHONE_CHANGE       
+FLAG_EMAIL                   
+FLAG_PHONE                   
+FLAG_CONT_MOBILE             
+FLAG_WORK_PHONE              
+FLAG_EMP_PHONE               
+FLAG_MOBIL                   
+# Client - locations
+REGION_POPULATION_RELATIVE
+LIVE_CITY_NOT_WORK_CITY      
+REG_CITY_NOT_WORK_CITY       
+REG_CITY_NOT_LIVE_CITY       
+LIVE_REGION_NOT_WORK_REGION  
+REG_REGION_NOT_WORK_REGION   
+REG_REGION_NOT_LIVE_REGION   
+REGION_RATING_CLIENT_W_CITY  
+REGION_RATING_CLIENT         
+# IDK - but has good distribution
+EXT_SOURCE_3                 
+EXT_SOURCE_2                 
+# sorround social circle (int -> almost 0)
+DEF_60_CNT_SOCIAL_CIRCLE     
+OBS_60_CNT_SOCIAL_CIRCLE     
+DEF_30_CNT_SOCIAL_CIRCLE     
+OBS_30_CNT_SOCIAL_CIRCLE     
+# IDK, maybe score (int value)
+AMT_REQ_CREDIT_BUREAU_QRT    
+AMT_REQ_CREDIT_BUREAU_MON    
+AMT_REQ_CREDIT_BUREAU_WEEK   
+AMT_REQ_CREDIT_BUREAU_DAY    
+AMT_REQ_CREDIT_BUREAU_HOUR   
+# Documents
+DOCUMENTS_COUNT -> sum(all documents flags)
+# Client ID change
+DAYS_REGISTRATION
+DAYS_ID_PUBLISH              
+# 
+HOUR_APPR_PROCESS_START      
+WEEKDAY_APPR_PROCESS_START   
+AMT_REQ_CREDIT_BUREAU_YEAR
